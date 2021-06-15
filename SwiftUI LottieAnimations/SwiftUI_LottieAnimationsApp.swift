fe01e014305f2a8ef6bec2b0fc3fb4f3b4033d5c
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-15 13:33:23 +0800

    Initial Commit

---
+++ SwiftUI LottieAnimations/SwiftUI_LottieAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LottieAnimationsApp.swift
+//  SwiftUI LottieAnimations
+//
+//  Created by Amben on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LottieAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
